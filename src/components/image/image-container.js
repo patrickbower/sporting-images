812d02b1ccf0337fa0bd81fe286888a5bebe7f47
Patrick Bower <bower.patrick@gmail.com>
2020-07-17 12:29:20 +0100

    feat: mock data
    save on request limit and use mock data

--- src/components/image/image-container.js
+++ src/components/image/image-container.js
@@ -1,19 +1,22 @@
 import React, {useState, useEffect} from "react";
 import ImageView from "./image-view";
-import RandomImage from '../../utils/random-image';
+// import RandomImage from '../../utils/random-image';
+import mockImage from './mock';
  
 const ImageContainer = () => {
   const [image, setImage] = useState(undefined);
   const [error, setErrors] = useState(false);
-  const searchSettings = {
-    query: "sports",
-    orientation: "landscape" 
-  }
+  // const searchSettings = {
+  //   query: "sports",
+  //   orientation: "landscape" 
+  // }
   
   useEffect(() => {
-    RandomImage(searchSettings)
-      .then(res => setImage(res))
-      .catch(err => setErrors(err));
+    // RandomImage(searchSettings)
+    //   .then(res => setImage(res))
+    //   .catch(err => setErrors(err));
+    // setImage({urls: {full: "https://dummyimage.com/600x400/000/fff"}})
+    setImage(mockImage);
   }, []);
 
 
---
+++ src/components/image/mock.js
@@ -0,0 +1,82 @@
+const image = {
+  "id": "KgYMstXJJzA",
+  "created_at": "2019-09-09T16:43:56-04:00",
+  "updated_at": "2019-11-28T00:08:35-05:00",
+  "promoted_at": null,
+  "width": 4160,
+  "height": 2340,
+  "color": "#EAEFF1",
+  "description": "Wimbledon",
+  "alt_description": "open field stadium",
+  "urls": {
+    "raw": "https://images.unsplash.com/photo-1568061813276-446d75348529?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE0ODcxN30",
+    "full": "https://images.unsplash.com/photo-1568061813276-446d75348529?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0ODcxN30",
+    "regular": "https://images.unsplash.com/photo-1568061813276-446d75348529?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE0ODcxN30",
+    "small": "https://images.unsplash.com/photo-1568061813276-446d75348529?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0ODcxN30",
+    "thumb": "https://images.unsplash.com/photo-1568061813276-446d75348529?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE0ODcxN30"
+  },
+  "links": {
+    "self": "https://api.unsplash.com/photos/KgYMstXJJzA",
+    "html": "https://unsplash.com/photos/KgYMstXJJzA",
+    "download": "https://unsplash.com/photos/KgYMstXJJzA/download",
+    "download_location": "https://api.unsplash.com/photos/KgYMstXJJzA/download"
+  },
+  "categories": [],
+  "likes": 62,
+  "liked_by_user": false,
+  "current_user_collections": [],
+  "sponsorship": null,
+  "user": {
+    "id": "HAV15Oh93G4",
+    "updated_at": "2020-06-28T04:27:04-04:00",
+    "username": "carlo_bazzo",
+    "name": "Carlo Bazzo",
+    "first_name": "Carlo",
+    "last_name": "Bazzo",
+    "twitter_username": "carlobazzo",
+    "portfolio_url": null,
+    "bio": null,
+    "location": "Treviso, Veneto, IT",
+    "links": {
+      "self": "https://api.unsplash.com/users/carlo_bazzo",
+      "html": "https://unsplash.com/@carlo_bazzo",
+      "photos": "https://api.unsplash.com/users/carlo_bazzo/photos",
+      "likes": "https://api.unsplash.com/users/carlo_bazzo/likes",
+      "portfolio": "https://api.unsplash.com/users/carlo_bazzo/portfolio",
+      "following": "https://api.unsplash.com/users/carlo_bazzo/following",
+      "followers": "https://api.unsplash.com/users/carlo_bazzo/followers"
+    },
+    "profile_image": {
+      "small": "https://images.unsplash.com/profile-1568839497929-82751b0547eeimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+      "medium": "https://images.unsplash.com/profile-1568839497929-82751b0547eeimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+      "large": "https://images.unsplash.com/profile-1568839497929-82751b0547eeimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+    },
+    "instagram_username": null,
+    "total_collections": 0,
+    "total_likes": 0,
+    "total_photos": 11,
+    "accepted_tos": true
+  },
+  "exif": {
+    "make": "Motorola",
+    "model": "MotoG3",
+    "exposure_time": "1/169",
+    "aperture": "2",
+    "focal_length": "3.6",
+    "iso": 50
+  },
+  "location": {
+    "title": "Wimbledon, Londra, Regno Unito",
+    "name": "Wimbledon, Londra, Regno Unito",
+    "city": null,
+    "country": "Regno Unito",
+    "position": {
+      "latitude": 51.4183389,
+      "longitude": -0.2206288
+    }
+  },
+  "views": 137310,
+  "downloads": 489
+}
+
+export default image;
