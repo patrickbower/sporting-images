6951573ea3d433defe346e45dfce02613a60d1de
Patrick Bower <bower.patrick@gmail.com>
2020-07-20 15:23:39 +0100

    feat: onload animation
    add basic animation to stage components

--- src/components/title/title-view.jsx
+++ src/components/title/title-view.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Styles from "./title.module.css";
 
 const Title = (props) => {
   return (
-    <h2 className={Styles.title}>
+    <motion.h1
+      className={Styles.title}
+      src={props.src}
+      alt={props.alt}
+      positionTransition
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
+    >
       {props.description ? props.description : "Untitled"}
-    </h2>
+    </motion.h1>
   );
 };
 
